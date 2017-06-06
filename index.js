94107b54d1df062c9e2de07e5ed51f6c75d164b0
doulighan <doulighan@gmail.com>
2017-06-06 15:38:08 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+
+const couponLocations = [
+  { room: 'Living room', amount: 5 },
+  { room: 'Kitchen', amount: 2 },
+  { room: 'Bathroom', amount: 1 },
+  { room: 'Master bedroom', amount: 7 },
+];
+ 
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+ 
+console.log(reduce(couponLocations, couponCounter, 0));
+
+function reduce(collection, callback, initialValue) {
+  let result = initialValue
+
+  collection.forEach((product, index) => {
+      result = callback(result, product, index, collection)
+  })
+  return result
+}
+
